710d647e81154fcd63bae375f4fc192ad0460530
JAMES LORENZ SANTOS <113660111+jamessuuu@users.noreply.github.com>
2024-03-11 04:29:04 +0000

    delete and check item done!!!!!!!

--- src/App.js
+++ src/App.js
@@ -7,15 +7,20 @@ import { useState } from "react";
 function App() {
   const [items, setItems] = useState([]);
 
-  function handleAddItem(item){
-    setItems(items=>[...items, item])
+  function handleAddItem(item) {
+    setItems((items) => [...items, item]);
     console.log(items);
   }
+
+  // function handleDeleteItem(id) {
+  //   setItems((items) => items.filter((item) => item.id !== id));
+  // }
+  // handleDeleteItem dapat nasa Form.js na kasi yun yung parent ng List component
 
   return (
     <div className="App">
       <Header />
-      <Form onAddItem= {handleAddItem}/>
+      <Form onAddItem={handleAddItem} />
     </div>
   );
 }
